cb073d86c54923d337e21283469b2e11f7c6a94e
Heln Zhao <pythonicrane@outlook.com>
2018-03-15 22:43:56 +0800

    纹理

---
+++ 1.6.1.hello_texture/hello_texture.cpp
@@ -0,0 +1,157 @@
+/*--------------------------------------------
+* 计算机图形学-LearnOpenGL-1.3.入门-纹理
+* 画一个矩形
+* 添加纹理
+* @author ZhaoHeln 2018年3月15日13:14:34
+---------------------------------------------*/
+
+#include<glad/glad.h>//应放在最前面，其包含了gl头文件
+#include<GLFW/glfw3.h>
+#include"../common/shader/include/shader_s.h"
+#include "../common/others/include/stb_image.h"
+
+#include<iostream>
+
+/*settings*/
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+/*---------------------------------------------------------------------------------
+* 窗口重绘函数
+* 当用户改变窗口的大小的时候，视口也应该被调整。
+* 我们可以对窗口注册一个回调函数(Callback Function)，它会在每次窗口大小被调整的时候被调用。
+*/
+void reshapeWindow(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+/*-----------
+* 键盘响应事件
+*/
+void processInput(GLFWwindow * window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+/*-----------
+* 主函数入口
+*/
+int main()
+{
+	/*GLFW初始化*/
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	/*创建窗口*/
+	GLFWwindow * window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "1.2.hello_tringle", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, reshapeWindow);
+
+	/*GLAD初始化*/
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	/*着色器*/
+	Shader myShader("shader.vs", "shader.fs");
+
+
+	/*顶点操作*/
+	float vertices[] = {
+		// positions          // colors           // texture coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+	};
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
+	};
+
+	/*生成VAO对象*/
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	/*坐标属性*/
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	/*颜色属性*/
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	/*纹理顶点属性*/
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	/*纹理对象*/
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture); 
+	/*设置纹理S，T轴属性*/
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	/*设置缩放属性*/
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	/*打开纹理图片*/
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load(std::string("D:/workspace/VS2017/MyLearnOpenGL/resources/textures/container.jpg").c_str(), &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);//生成纹理map
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	glBindTexture(GL_TEXTURE_2D,0);//关闭纹理属性绑定
+
+	/*主循环*/
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		myShader.use();
+		glBindVertexArray(VAO);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//线框模式，默认为填充模式
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	/*关闭程序前的内存清理*/
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glfwTerminate();//退出清理GLFW resources
+	return 0;
+}
---
+++ 1.6.1.hello_texture/shader.fs
@@ -0,0 +1,11 @@
+﻿#version 330 core
+in vec3 ourColor;
+in vec2 TexCoord;
+out vec4 FragColor;
+uniform sampler2D myTexture;
+
+void main()
+{
+	FragColor = texture(myTexture,TexCoord);
+	//FragColor = texture(myTexture,TexCoord)*vec4(ourColor, 1.0f);纹理和颜色的混合
+}
